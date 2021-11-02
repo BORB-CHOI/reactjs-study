b2bc358c8fa254b34b19014759b36e18405d26d8
dan-2ee <65989401+dan-2ee@users.noreply.github.com>
2021-11-02 16:42:08 +0900

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+import Name from './Name'
+import React, {Component} from "react";
+import {render} from "@testing-library/react";
+
+
+function App() {
+    render(); {
+        return (
+            <div>
+                <Name />
+            </div>
+        );
+    }
+}
+
+export default App;
